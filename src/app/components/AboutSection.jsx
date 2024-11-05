d509efe5a73bcdb06656eeb9119a7e53b75a8f01
taha-ishaq <tahaishaq7346@gmail.com>
2024-11-06 04:28:40 +0500

    Jk

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -10,7 +10,7 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Branding</li>
-        <li>LeaderShip</li>
+        <li>Leadership</li>
         <li>Social Media Management</li>
         <li>Graphics Designing</li>
         <li>UI/UX Designing</li>
@@ -23,9 +23,8 @@ const TAB_DATA = [
     id: "education",
     content: (
       <ul className="list-disc pl-2">
-        <li>B.sc Double Math & Physics</li>
+        <li>B.Sc. in Double Math & Physics</li>
         <li>Graphics Designing at Wapexp Institute</li>
-        
       </ul>
     ),
   },
@@ -35,7 +34,7 @@ const TAB_DATA = [
     content: (
       <ul className="list-disc pl-2">
         <li>Dtek Internship in Graphics Designing</li>
-        <li>Dtek Job Certficate in Graphics Designing</li>
+        <li>Dtek Job Certificate in Graphics Designing</li>
       </ul>
     ),
   },
@@ -54,33 +53,35 @@ const AboutSection = () => {
   return (
     <section className="text-white" id="about">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
-        <Image src="/images/about-image.png" width={500} height={500}  alt="A description of the image" />
+        <Image
+          src="/images/about-image.png"
+          width={500}
+          height={500}
+          alt="A description of the image"
+        />
         <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
           <p className="text-base lg:text-lg">
-          I'm a creative and strategic professional with expertise in Branding, Leadership, Social Media Management, Graphic Design, UI/UX Design, and Sales & Marketing. I specialize in building strong brand identities, leading teams, and crafting user-friendly digital experiences. My goal is to help businesses grow by creating impactful designs and marketing strategies that resonate with their audiences and drive results.
+            I&apos;m a creative and strategic professional with expertise in Branding, Leadership, Social Media Management, Graphic Design, UI/UX Design, and Sales & Marketing. I specialize in building strong brand identities, leading teams, and crafting user-friendly digital experiences. My goal is to help businesses grow by creating impactful designs and marketing strategies that resonate with their audiences and drive results.
           </p>
           <div className="flex flex-row justify-start mt-8">
             <TabButton
               selectTab={() => handleTabChange("skills")}
               active={tab === "skills"}
             >
-              {" "}
-              Skills{" "}
+              Skills
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("education")}
               active={tab === "education"}
             >
-              {" "}
-              Education{" "}
+              Education
             </TabButton>
             <TabButton
               selectTab={() => handleTabChange("certifications")}
               active={tab === "certifications"}
             >
-              {" "}
-              Certifications{" "}
+              Certifications
             </TabButton>
           </div>
           <div className="mt-8">
